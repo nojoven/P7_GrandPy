edd3b255a9bd315da89a9d9971262f27b71cabd6
cedricjoseph <work.cedricjoseph@gmail.com>
2020-02-08 21:18:09 +0100

    I have chosen a better UI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template("chat.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
